1e910c408b8e6ddc9474ba9be1134a21bdf9b030
Green-EggsAndSam <126127300+Green-EggsAndSam@users.noreply.github.com>
2025-12-18 11:11:17 -0500

    comments

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,9 +2,11 @@ import {app, BrowserWindow} from 'electron';
 import path from 'path';
 
 app.on("ready", ()=> {
+    //creates mainWindow
     const mainWindow = new BrowserWindow();
-    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'/*Runs on location of main html for window(main)*/));
 
-    const secWindow = new BrowserWindow();
-    secWindow.loadFile(path.join(app.getAppPath(),'/dist-react/manager.html'));
+    //creates managerWindow
+    const managerWindow = new BrowserWindow();
+    managerWindow.loadFile(path.join(app.getAppPath(),'/dist-react/manager.html'/*Runs on location of main html for window(manager)*/));
 })
