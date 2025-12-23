9de8963e046db10565154d82a1f9ef18a4326114
Green-EggsAndSam <126127300+Green-EggsAndSam@users.noreply.github.com>
2025-12-23 13:22:16 -0500

    toggle-able dev tools

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,27 @@
 import {app, BrowserWindow} from 'electron';
 import path from 'path';
 
+const devTools = true;//dev tools, inspect element
+
 app.on("ready", ()=> {
     //creates mainWindow
-    const mainWindow = new BrowserWindow();
+
+    const mainWindow = new BrowserWindow({
+        autoHideMenuBar: true,
+
+        webPreferences: {
+            devTools: devTools
+        }
+    });
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'/*Runs on location of main html for window(main)*/));
 
+
     //creates managerWindow
-    const managerWindow = new BrowserWindow();
+    const managerWindow = new BrowserWindow({
+        autoHideMenuBar: true,
+        webPreferences: {
+            devTools: devTools
+        }
+    });
     managerWindow.loadFile(path.join(app.getAppPath(),'/dist-react/manager.html'/*Runs on location of main html for window(manager)*/));
 })
