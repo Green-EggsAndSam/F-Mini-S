3b9938d3bc4d7873c1c7741a39aa9159270ab96d
Green-EggsAndSam <126127300+Green-EggsAndSam@users.noreply.github.com>
2026-02-07 16:37:55 -0500

    match generator function almost finsihed, new update json scipt in utils, for generator need to look over final changes and at least 12 team counter needs to be added.

--- src/electron/jsonUtils.ts
+++ src/electron/jsonUtils.ts
@@ -3,3 +3,10 @@ import fs from 'fs'
 export function jsonToString(path : string): string{
     return fs.readFileSync(path,'utf-8');
 }
+
+export function safeWriteJSON(filename : string, data : unknown) {
+    const tempFile = filename + ".tmp";
+
+    fs.writeFileSync(tempFile, JSON.stringify(data, null, 2));
+    fs.renameSync(tempFile, filename);
+}
--- src/electron/matchMaker.ts
+++ src/electron/matchMaker.ts
@@ -1,16 +1,20 @@
 import fs from 'fs';
 import { jsonToString } from './jsonUtils.js'
+import { safeWriteJSON } from './jsonUtils.js'
 
 const matchSchedulePath = './src/electron//matchSchedule.json'
 const teamsPath = './src/electron/teams.json'
 
-let teams = JSON.parse(jsonToString(teamsPath));
-let matchSchedule = JSON.parse(jsonToString(matchSchedulePath));
+//teams -> JSON.parse(jsonToString(teamsPath))
+//matchSchedule -> JSON.parse(jsonToString(matchSchedulePath))
 
 export function print(){
     setInterval(()=> {
-        console.log(matchOptions(5));
-    }, 1000);
+        const arr = new Array(2);
+        arr[0] = matchOptions(1)[0];
+        arr[1] = matchOptions(1)[1];
+        generateMatchSchedule(arr[0],arr[1]);
+    }, 10000);
 }
 
 /**
@@ -22,8 +26,8 @@ export function matchOptions( optionsNum : number){
     const teams = JSON.parse(jsonToString(teamsPath));
 
     const optionAmount = optionsNum;
-    let roundsPerTeamOptions = new Array(optionAmount).fill(1);
-    let roundsTotalOptions = new Array(optionAmount);
+    const roundsPerTeamOptions = new Array(optionAmount).fill(1);
+    const roundsTotalOptions = new Array(optionAmount);
     
 
     for(let i = 0; i < optionAmount; i++){
@@ -49,15 +53,76 @@ export function matchOptions( optionsNum : number){
 /**
  * Generates the match schecdule for the matchs and updates JSON
  */
-export function generateMatchSchedule(){
+export function generateMatchSchedule(totalMatches : number, matchesPerTeam : number){
+
     const teams = JSON.parse(jsonToString(teamsPath));
-    const matchScheduleJSON = JSON.parse(jsonToString(matchSchedulePath));
-    let result = "[\n\n\t";
 
-    let teamNumArr = new Array(teams.length);
+    type Match = {
+        number: number,
+        redAlliance: number[],
+        blueAlliance: number[]
+    };
 
+    type MatchFile = Match[];
+
+    const matches : MatchFile = [];
+
+    /* Example
+    matches.push({
+        number: 2,
+        redAlliance: [11,12,13],
+        blueAlliance: [11,12,13]
+    });
+    */
+
+    const teamNumArr = new Array(teams.length);
+    //Needs an if statement to check if there are less than 12 teams, if less some teams will have back to back matches
     for(let i = 0; i < teamNumArr.length; i++) { teamNumArr[i] = teams[i].number; }
 
-    let teamRoundsArr = new Array(teams.length).fill(0);
-    let teamLastRoundArr = new Array(teams.length).fill(false);
+    const teamRoundsArr = new Array(teams.length).fill(0);
+    const teamLastRoundArr = new Array(teams.length).fill(false);
+
+    let end = false;
+    let matchCounter = 1;
+
+    while(!end){
+
+        //First three teams are red alliance, Second three are blue alliance
+        const alliance = new Array(6);
+
+        for(let i = 0; i < 6; i++){
+
+            let enda = false;
+
+            while(!enda){
+                const ranTeam = Math.floor(Math.random()*(teams.length+1));
+                
+                if(teamRoundsArr[ranTeam] < matchesPerTeam && !teamLastRoundArr[ranTeam] && alliance.indexOf(teamNumArr[ranTeam]) == -1){
+                    
+                    alliance[i] = teamNumArr[ranTeam];
+                    teamRoundsArr[i]++;
+                    enda = true;
+                }
+            }
+        }
+        
+        teamLastRoundArr.fill(false);
+
+        for(let i = 0; i < 6; i++){
+            teamLastRoundArr[teamNumArr.findIndex(alliance[i])] = true;
+        }
+
+        matches.push({
+        number: matchCounter,
+        redAlliance: [alliance[0],alliance[1],alliance[2]],
+        blueAlliance: [alliance[3],alliance[4],alliance[5]]
+        });
+
+        matchCounter++;
+
+        if(matchCounter > totalMatches)
+            end = true;
+    }
+
+    safeWriteJSON(matchSchedulePath, matches);
 }
